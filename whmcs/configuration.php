d2a47ca702becd88af200532779a0dd01aced7bc
cuiminghao8 <cuiminghao8@gmail.com>
2017-01-10 11:44:35 +0800

    Update configuration.php

--- whmcs/configuration.php
+++ whmcs/configuration.php
@@ -1,9 +1,9 @@
 <?php
-$license = 'Hoya19890710';
+$license = '';
 $db_host = 'localhost';
 $db_port = '';
 $db_username = 'root';
-$db_password = 'Hoya19890710';
+$db_password = '';
 $db_name = 'shadowsocks';
 $cc_encryption_hash = 'ltjsm13irW6ip8I2zpGsHvUpwmvCXc08v9qH0kQi6MqmKYfaeLtL7e1tqmWdoNcK';
 $templates_compiledir = 'templates_c';
